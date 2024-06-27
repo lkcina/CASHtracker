b3c51034f2177ea7f49d32d6477440baba34b312
Landon Cina <lkcina6@gmail.com>
2024-06-27 10:07:01 -0400

    Set variables and initial setDisplay func

---
+++ script.js
@@ -0,0 +1,26 @@
+const editCatBtn = document.getElementById("edit-cat-btn");
+const editWindow = document.getElementById("edit-window");
+const budgetContainer = document.getElementById("budget-container");
+const budgetPlaceholder = document.getElementById("budget-placeholder");
+const toolbar = document.getElementById("toolbar");
+const newBudgetBtn = document.getElementsByClassName("new-budget-btn");
+const budgetName = document.getElementById("budget-name");
+const addReceiptBtn = document.getElementById("add-receipt-btn");
+
+let isCurrentBudget = false;
+const categories = [];
+
+setDisplay();
+
+function setDisplay() {
+    if (isCurrentBudget) {
+        toolbar.style.display = "block"
+        budgetContainer.style.display = "block";
+        budgetPlaceholder.style.display = "none";
+    }
+}
+
+
+function editCategories() {
+
+};
